89f0ca4a1b77a64358462e5f39750c64ed641603
juan.salinas <juan.salinas@travelx.io>
2024-02-15 21:23:11 -0300

    feat: CRUD, users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { StatusCodes } from "http-status-codes";
-import { createUser, getAllUsers } from "@service/UserService";
+import { createUser, getAllUsers, modifyUser } from "@service/UserService";
 import { User } from "@model/user"
 
 export async function POST(req: NextRequest) {
@@ -11,6 +11,28 @@ export async function POST(req: NextRequest) {
   } catch (error) {
       console.error(error);
       return NextResponse.error();
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    const searchParams = req.nextUrl.searchParams
+    const user = await req.json();
+    const id: string = searchParams.get('id')
+    console.log(id);
+    const resp = await modifyUser(user, id);
+    return NextResponse.json(resp,
+      {
+        status: StatusCodes.OK
+      })
+
+  } catch (error) {
+    return NextResponse.json(
+      error.message,
+      {
+        status: StatusCodes.NOT_FOUND
+      }
+    );
   }
 }
 
--- src/app/backend/service/UserService.ts
+++ src/app/backend/service/UserService.ts
@@ -16,3 +16,29 @@ export async function getAllUsers() : Promise<User[]> {
     await connectToDB();
     return await UserRepository.find();
 }
+
+
+export async function findUserById(id:string) {
+    await connectToDB();
+    const user = await UserRepository.findOne({_id:{$eq:id}});
+    if(user)
+        return user;
+    else
+        throw new Error(`Threre's no user with id: ${id}`)
+}
+
+export async function modifyUser(user:User,id:string) {
+    const filter={_id:{$eq:id}}
+    try {
+        await connectToDB();
+        let storedUser = await UserRepository.findOne(filter)
+        console.log(typeof storedUser)
+        console.log(storedUser)
+        if(storedUser)
+            return await UserRepository.replaceOne(filter,user);
+        else
+            throw new Error(`There's no user with id: ${id}`)    
+    } catch (error) {
+        throw new Error(error.message);
+    }  
+}

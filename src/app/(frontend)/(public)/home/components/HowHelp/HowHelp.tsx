06cf8dbfd36c070ea7bd1206b144a64122ad1286
Santiago Iannello <santiagoiannellot@gmail.com>
2024-04-03 20:37:30 -0300

    landing

--- src/app/(frontend)/(public)/home/components/HowHelp/HowHelp.tsx
+++ src/app/(frontend)/(public)/home/components/HowHelp/HowHelp.tsx
@@ -26,7 +26,7 @@ function HowHelp() {
           </div>
         </Reveal>
         <Reveal>
-          <div className="flex items-end w-full h-96 mb-12 rounded-lg bg-cover bg-center shadow-xl bg-fixed" style={{backgroundImage: 'url(/landing/howhelp/fondo_howhelp.jpg)'}}>
+          <div className="flex items-end w-full h-96 mb-12 rounded-lg bg-cover bg-bottom shadow-xl bg-fixed" style={{backgroundImage: 'url(/landing/howhelp/fondo_howhelp.jpg)'}}>
             <div className="w-full h-4/5 py-4 px-4 bg-green_greenplus text-xs bg-opacity-80 rounded-lg md:h-2/5 md:rounded-b-lg md:rounded-t-none md:py-4 md:px-14 md:text-sm">
               <p className="text-white text-justify">
                 Greenplus emerge como la solución innovadora y eficaz para el impacto devastador de las emisiones de carbono en nuestro medio ambiente. En nuestra plataforma, los productores pueden dar un paso revolucionario al tokenizar los carbonos capturados, permitiendo así que empresas y personas compensen su huella de carbono de manera más accesible y transparente. 
